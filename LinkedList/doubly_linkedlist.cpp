1db190fe078080e61182046960dbf27b97a3fa25
Viral Baitule <viral.baitule@gmail.com>
2017-03-04 16:23:55 -0800

    added function definations and ran test cases- final code

--- LinkedList/doubly_linkedlist.cpp
+++ LinkedList/doubly_linkedlist.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include "string"
 #include <iomanip>
+#include <stdlib.h>
 using namespace std;
 
 struct Node{
@@ -12,19 +13,63 @@ struct Node{
 };
 
 void insert_at_begining(Node **head , int num){
-
+	Node * newnode=new Node();
+	newnode->data=num;
+	newnode->next=*head;
+	newnode->prev=NULL;
+	if(*head!=NULL){
+		(*head)->prev=newnode;
+	}
+	*head=newnode;
 }
 
 void insert_at_position (Node **head){
+	cout<<"Enter the postion and number\n";
+	int pos,num;
+	cin>>pos>>num;
+	Node * temp=*head;
+	Node *newnode=new Node();
+	newnode->data=num;
+	if (pos==1){
+		insert_at_begining(&*head,num);
+		return;
+	}
+	else{
+		for (int i=0;i<pos-2;i++){
+			temp=temp->next;
+		}
+		newnode->next=temp->next;
+		newnode->prev=temp;
+		temp->next=newnode;
+		newnode->next->prev=newnode;
+	}
 
 }
 
 void delete_at_positon(Node **head){
+	cout<<"Enter the position\n";
+	int pos;
+	cin>>pos;
+	Node * temp=*head;
+	for (int i=0;i<pos-1;i++){
+		temp=temp->next;
+	}
+	temp->prev->next=temp->next;
+	temp->next->prev=temp->prev;
 
 }
 
 void reverseprint(Node * head){
-	
+	Node * temp=head;
+	while(temp->next!=NULL){
+		temp=temp->next;
+	}
+	while (temp->prev!=NULL){
+		cout<<temp->data<<"	";
+		temp=temp->prev;
+	}
+	cout<<temp->data<<endl;
+
 }
 
 void printlist(Node *head){
@@ -33,6 +78,7 @@ void printlist(Node *head){
 		cout<<temp->data<<"	";
 		temp=temp->next;
 	}
+	cout<<endl;
 }
 
 int main(int argc, char * agrv[]){
@@ -51,6 +97,9 @@ int main(int argc, char * agrv[]){
 		newnode->data=data[i];
 		newnode->next=head;
 		newnode->prev=NULL;
+		if(head!=NULL){
+			head->prev=newnode;
+		}
 		head=newnode;
 	}
 	while (choice!=10){
@@ -71,7 +120,7 @@ int main(int argc, char * agrv[]){
 			{
 				int num;
 				cout<<"Enter the No.\n";
-				cin<<num;
+				cin>>num;
 				insert_at_begining(&head,num);
 				break;
 			}
