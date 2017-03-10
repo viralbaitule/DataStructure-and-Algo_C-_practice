ee2c3245eae0333eeae1ee73ecac9fca737b3a45
Viral Baitule <viral.baitule@gmail.com>
2017-03-09 17:31:09 -0800

    final program

--- Sorting/selection_sort.cpp
+++ Sorting/selection_sort.cpp
@@ -7,22 +7,40 @@ class sorting{
 
 public:
 	void print_arr(int *,int);
+
 	void selection_sort(int *, int );
+
 };
 
 void sorting::selection_sort(int * arr, int len){
-	cout<<"selection sort\n";
+	for (int i=0; i<len-1 ;i++){
+		int arrmin=i;
+		for (int j=i+1;j<len;j++){
+			if (arr[j]<arr[arrmin]){
+				arrmin=j;
+			}
+			int temp=arr[i];
+			arr[i]=arr[arrmin];
+			arr[arrmin]=temp;
+		}
+	}
 
 }
 
 void sorting::print_arr(int * arr,int len){
-	cout<<"print \n";
+	for (int i=0; i<len;i++){
+		cout<<arr[i]<<"	";
+	}
+	cout<<endl;
 }
 
 int main(){
 	int arr[]={5,3,6,1,8,9};
 	sorting s;
+	cout<<"initial array :	";
+	s.print_arr(arr,6);
 	s.selection_sort(arr,6);
+	cout<<"Sorted Array:	";
 	s.print_arr(arr,6);
 
 }
